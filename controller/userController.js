45d1417e7fe0409978801767caf7e7f561d6e51e
shivani awasthi <shvaniawsthi001@gmail.com.com>
2023-01-24 00:34:57 +0530

    ff

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,3 @@
-
 import asyncHandler from "express-async-handler"
 import { generateToken } from "../utils/generateToken.js"
 import User from "../Models/UserModel.js"
@@ -8,7 +7,7 @@ const authUser =asyncHandler(async(req,res)=>{
 
      const user = await User.findOne({email})
      if(user &&(await user.matchPassword(password))){
-     res.status(200).json({
+     res.json({
         _id:user._id,
         name:user.name,
         email:user.email,
@@ -18,16 +17,44 @@ const authUser =asyncHandler(async(req,res)=>{
      
      }
      else{
-        res.status(404).json({
-         error:"Invalid Credentials"
-        })
-      //   throw new Error("Invalid Credentials")
+        res.status(404)
+        throw new Error("Invalid Credentials")
         
      }
 
-   //   res.send({email,password})
+     res.send({email,password})
 
 })
+
+const registerUser=asyncHandler(async(req,res)=>{
+   const {name,email,password} = req.body
+
+   const userExists = await User.findOne({email})
+   
+   if(userExists){
+      res.status(400)
+      throw new Error("User Already Exists..")
+      }
+   
+   const user=await User.create({name,email,password})
+
+   if(user){
+      res.status(201).json({
+         _id:user._id,
+        name:user.name,
+        email:user.email,
+        isAdmin:user.isAdmin,
+        token:generateToken(user._id)
+
+      })
+   }else{
+      res.status(400)
+      throw new Error (`Invalid user data`)
+   }
+
+
+})
+
 
 
 const getUserProfile =asyncHandler(async(req,res)=>{
@@ -53,5 +80,4 @@ const getUserProfile =asyncHandler(async(req,res)=>{
    // res.send({email,password})
 
 })
-
-export {authUser,getUserProfile}
+export {authUser,getUserProfile,registerUser}
